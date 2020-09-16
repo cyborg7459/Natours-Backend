29d8a9084c2b2ef9290ffb1c6b024c98b51a1690
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-09-16 11:58:58 +0530

    Connect to database

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+// Setting up the .env file
 const dotenv = require('dotenv');
 dotenv.config({path : './config.env'});
-const app = require('./app');
 
+// Requiring necessary modules
+const app = require('./app');
+const mongoose = require('mongoose');
+
+// Database connection
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Successfully connected to database')
+})
+
+// Server start
 const port = process.env.PORT||3000;
 app.listen(port, () => {
     console.log('Server running on port 3000');
