2be7fbcdd9d55009ef907128b4aebfaa05b9aa9d
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-10-07 10:37:59 +0530

    Handle undefined routes

--- app.js
+++ app.js
@@ -16,5 +16,12 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req,res,next) => {
+    res.status(404).json({
+        status: 'Failure',
+        message: `Can't find ${req.originalUrl} on this server !!!`
+    })
+    next();
+})
 
 module.exports = app;
