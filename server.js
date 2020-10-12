419bb1b99b734dbca336284f580609ff020b199f
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-10-12 21:19:56 +0530

    handle uncaught exceptions

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ const server = app.listen(port, () => {
 })
 
 process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    })
+})
+
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
     server.close(() => {
         process.exit(1);
     })
