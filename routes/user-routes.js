34d17a7ad62109102d172f5f7625666ceeaa53a8
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-10-13 01:59:19 +0530

    Create basic signup route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user-controller');
+const authController = require('../controllers/auth-controller');
+
+router.post('/signup', authController.signup);
 
 router.route('/')
     .get(userController.getAllUsers)
