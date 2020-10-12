543f5cbba43b76a6b4dcaf8a2a72464258de0c55
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-10-13 01:59:47 +0530

    create the auth controller

---
+++ controllers/auth-controller.js
@@ -0,0 +1,16 @@
+const User = require('../models/userModel');
+
+exports.signup = async (req,res, next) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                user: newUser
+            }
+        })
+    }
+    catch (err) {
+        next(err);
+    }
+}
