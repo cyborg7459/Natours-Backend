7536f7b1b181767259e5b9dd2cca744fcec455e4
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-10-18 20:13:15 +0530

    Refactor middleware and add HTTP security headers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 // Requiring functions and middleware
 const tourRouter = require('./routes/tour-routes');
@@ -11,7 +12,8 @@ const globalErrorHandler = require('./controllers/error-controller');
 const appError = require('./utils/appError');
 
 // MIDDLEWARE
-if(process.env.NODE_ENV === 'development')
+app.use(helmet());   // Security HTTP Headers
+if(process.env.NODE_ENV === 'development')      // Logging during development
     app.use(morgan('dev'));
 else 
     console.log('Running app in production mode');
@@ -21,11 +23,13 @@ const limiter = rateLimit({
     windowMs: 60*60*1000,
     message: 'Too many requests from this IP, please try again in an hour!'
 });
-app.use('/api', limiter);
-app.use(express.json());
+app.use('/api', limiter);       // Rate limiting
+app.use(express.json({
+    limit: '10kb'
+}));        // Body parser
+app.use(express.static('public'));      // Serving static files
 
 // Routes
-app.use(express.static('public'));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req,res,next) => {
