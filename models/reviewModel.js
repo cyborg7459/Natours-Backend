9476e733ac61aa9ce4b9fe6c035f7b53aed8c956
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-10-23 11:17:09 +0530

    Create review model

---
+++ models/reviewModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    review: {
+        type: String,
+        required: [true, 'Review can not be empty'],
+        trim: true
+    },
+    rating: {
+        type: Number,
+        max: [5, 'Rating can\'t be more than 5 stars'],
+        min: [0, 'Rating can\'t be less than zero']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date
+    },
+    forTour: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'Review must belong to a tour']
+    },
+    byUser: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Review must have a user']
+    }
+}, {
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
+})
+
+reviewSchema.pre('save', function(next) {
+    if(!(this.isModified('review') || this.isModified('rating')))
+        this.createdAt = Date.now();
+    else 
+        this.updatedAt = Date.now();
+    next();
+})
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
