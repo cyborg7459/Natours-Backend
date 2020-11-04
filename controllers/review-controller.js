7a1471bececfc86c6f274caf21480d940ff5717a
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-04 21:18:44 +0530

    Implement getAll factory function

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -1,26 +1,6 @@
 const Review = require('../models/reviewModel');
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = async (req,res,next) => {
-    try {
-        let filter = {};
-        if(req.params.tourId) {
-            filter = {forTour : req.params.tourId};
-        }
-        const reviews =  await Review.find(filter);
-        res.status(200).json({
-            status: 'success',
-            results: reviews.length,
-            data : {
-                reviews
-            }
-        });
-    }
-    catch (err) {
-        next(err);
-    }
-}
-
 exports.setTourUserID = (req,res,next) => {
     if(!req.body.byUser)
         req.body.byUser = req.user._id;
@@ -29,6 +9,7 @@ exports.setTourUserID = (req,res,next) => {
     next();
 }
 
+exports.getAllReviews = factory.getAll(Review);
 exports.getSingleReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
