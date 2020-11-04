67eb18b6eedce49e67a877dc94f1714fdf708cc7
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-04 21:31:57 +0530

    Implement getAll factory function

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -1,18 +1,18 @@
 const Tour = require('./../models/tourModel');
 const factory = require('./handlerFactory');
 
+exports.getAllTours = factory.getAll(Tour);
+exports.getSingleTour = factory.getOne(Tour, {path: 'reviews'});
+exports.addTour = factory.createOne(Tour);
+exports.updateTour =  factory.updateOne(Tour);
+exports.deleteTour = factory.deleteOne(Tour);
+
 exports.aliasTopTours = (req,res,next) => {
     req.query.limit = '5';
     req.query.sort = '-ratingsAverage,price';
     req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
     next();
 }
-
-exports.getAllTours = factory.getAll(Tour);
-exports.getSingleTour = factory.getOne(Tour, {path: 'reviews'});
-exports.addTour = factory.createOne(Tour);
-exports.updateTour =  factory.updateOne(Tour);
-exports.deleteTour = factory.deleteOne(Tour);
 
 exports.getTourStats = async (req,res) => {
     try {
