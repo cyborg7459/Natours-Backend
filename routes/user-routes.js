70b6ba6a84c3722f2aa0af8ae428ac1e2803e7de
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-06 10:51:16 +0530

    Add authorization

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -8,30 +8,18 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.get('/me', 
-    authController.protect,
-    userController.getMe,
-    userController.getSingleUser
-);
+// Middleware to protect all routes after this middleware
+router.use(authController.protect);
 
-router.patch('/updatePassword', 
-    authController.protect, 
-    authController.updatePassword
-);
+router.get('/me', userController.getMe, userController.getSingleUser);
+router.patch('/updatePassword', authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+router.patch('/deleteMe', userController.deleteMe);
 
-router.patch('/updateMe', 
-    authController.protect, 
-    userController.updateMe
-);
+// Middleware to restrict all the routes after this to only admins
+router.use(authController.restrictTo('admin'));
 
-router.patch('/deleteMe', 
-    authController.protect, 
-    userController.deleteMe
-);
-
-router.route('/')
-    .get(userController.getAllUsers)
-
+router.route('/').get(userController.getAllUsers);
 router.route('/:id')
     .get(userController.getSingleUser)
     .patch(userController.updateUser)
