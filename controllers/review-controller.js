a582debcc6e281149449e89a8fb955cf6d1284f7
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-06 11:31:02 +0530

    fixed bug

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -13,6 +13,8 @@ exports.setTourUserID = (req,res,next) => {
 exports.authorizeUpdateAndDelete = async (req,res,next) => {
     if(req.user.role === 'user') {
         const review = await Review.findById(req.params.id);
+        if(!review)
+            return next();
         if(!review.byUser._id.equals(req.user._id))
             next(new appError('You are not allowed to change or delete another user\'s review', 403));
     }
