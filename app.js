09c7795a407690c244d8f770b9f50e9f0a59d52d
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-12 11:45:25 +0530

    Basic pug templating

--- app.js
+++ app.js
@@ -59,7 +59,10 @@ app.use(hpp({
 
 // Routes
 app.get('/', (req,res) => {
-    res.status(200).render('base');
+    res.status(200).render('base', {
+        tour: 'The Forest Hiker',
+        user: 'Shreyash'
+    });
 })
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
