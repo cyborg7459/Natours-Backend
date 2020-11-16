0e69735048900eea0427301e125a8939bc47065b
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-16 10:54:55 +0530

    add the overview and tour page

--- app.js
+++ app.js
@@ -64,6 +64,18 @@ app.get('/', (req,res) => {
         user: 'Shreyash'
     });
 })
+app.get('/overview', (req,res) => {
+    res.status(200).render('overview', {
+        title: 'All tours'
+    });
+})
+app.get('/tour', (req,res) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker'
+    });
+})
+
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
