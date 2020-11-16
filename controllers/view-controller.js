f3b7c4090b6b84dbf8344078806cdf24e3fdb0a2
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-16 15:52:51 +0530

    Complete the tour page

--- controllers/view-controller.js
+++ controllers/view-controller.js
@@ -13,8 +13,18 @@ exports.getOverview = async (req,res, next) => {
     }
 }
 
-exports.getTour =  (req,res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker'
-    });
+exports.getTour = async (req,res,next) => {
+    try {
+        const tour = await Tour.findOne({slug: req.params.tourName}).populate({
+            path: 'reviews',
+            fields: 'review rating byUser'
+        });
+        res.status(200).render('tour', {
+            title: `${tour.name} Tour`,
+            tour
+        });
+    }
+    catch (err) {
+        return next(err);
+    }
 }
--- routes/view-routes.js
+++ routes/view-routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const viewController = require('../controllers/view-controller');
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tours/:tourName', viewController.getTour);
 
 module.exports = router;
