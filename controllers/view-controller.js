7d17f9280b5d9f517d7871900b768adfdd3285dc
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-16 13:39:59 +0530

    build the overview page

--- controllers/view-controller.js
+++ controllers/view-controller.js
@@ -1,7 +1,16 @@
-exports.getOverview =  (req,res) => {
-    res.status(200).render('overview', {
-        title: 'All tours'
-    });
+const Tour = require('../models/tourModel');
+
+exports.getOverview = async (req,res, next) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).render('overview', {
+            title: 'All tours',
+            tours
+        });
+    }
+    catch (err) {
+        return next(err);
+    }
 }
 
 exports.getTour =  (req,res) => {
