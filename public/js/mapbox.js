7285c7755e90505a435ec88f8ef7c23fc4e50dd4
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-11-30 14:42:20 +0530

    Implement login request

--- public/js/mapbox.js
+++
@@ -1,8 +0,0 @@
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiY3lib3JnNzQ1OSIsImEiOiJja2k0OGxmaTgwbXUzMzBxc29rcmp1amJwIn0.xRBe1Au0g7-JgXQHHq2Sfw';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-});
